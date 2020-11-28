dfa89bf21be9dafc5c4c14104c0ff2cb566ced78
Samantha Fok <samantha.fok@hotmail.com>
2020-11-28 21:02:26 +0000

    Created menu route to get menu page

--- routes/menu.js
+++ routes/menu.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const router  = express.Router();
+
+// module.exports = (db) => {
+  // VIEW MENU
+  router.get("/", (req, res) => {
+    console.log("reached the menu page")
+    res.render("menu");
+  });
+
+module.exports = router;
+
