52ab6d94e149a15c1752c753bb84a617907aabbb
RAFH82 <hrechkar@gmail.com>
2020-11-29 01:46:22 +0000

    Moved renders for "/" , "/login" , "/register" inside of (db) function in module.exports

--- routes/home.js
+++ routes/home.js
@@ -1,33 +1,28 @@
 const express = require('express');
 const router  = express.Router();
 
-// module.exports = (db) => {
-//   router.get("/login/:user_id", (req, res) => {
-//     db.query(`SELECT * FROM users;`)
-//       .then(data => {
-//         const users = data.rows;
-//         res.json({ users });
-//       })
-//       .catch(err => {
-//         res
-//           .status(500)
-//           .json({ error: err.message });
-//       });
-//   });
-//   return router;
-// };
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    res.render("home");
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
 
-// VIEW HOME
-router.get("/", (req, res) => {
-  res.render("home");
-});
+  router.get("/login", (req, res) => {
+    res.render("login");
+  });
 
-router.get("/login", (req, res) => {
-  res.render("login");
-});
+  router.get("/register", (req, res) => {
+    res.render("register");
+  });
+  return router;
+};
 
-router.get("/register", (req, res) => {
-  res.render("register");
-});
-
-module.exports = router;
