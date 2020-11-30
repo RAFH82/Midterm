0f183245c2a57eb67424f337ae48a1669cbcd27d
RAFH82 <hrechkar@gmail.com>
2020-11-30 21:47:58 +0000

    trying to get food order item to display

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const router  = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("checkout");
-});
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`
+    SELECT * FROM food_orders
+    JOIN orders ON orders.id = order_id
+    JOIN foods ON foods.id = food_id
+    JOIN users ON users.id = user_id
+    WHERE users.id = 1
+    ;`)
+      .then(data => {
+        const order = data.rows[0];
+        const name = order.name;
+        const time = order.order_date;
+        console.log(name, time);
+        res.json({ order });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
 
-router.get("/submit", (req, res) => {
-  res.render("confirmation");
-})
+// router.get("/", (req, res) => {
+//   res.render("checkout");
+// });
 
-module.exports = router;
+// // router.get("/submit", (req, res) => {
+// //   res.render("confirmation");
+// // })
+
+// module.exports = router;
