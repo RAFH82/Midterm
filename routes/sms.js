d9ff57c405f9eb9c579d27b3a65fb192b64862e7
RAFH82 <hrechkar@gmail.com>
2020-11-30 19:40:11 +0000

    initial commit to ryan branch

---
+++ routes/sms.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router  = express.Router();
+const Nexmo = require('nexmo');
+const nexmo = new Nexmo({
+  apiKey: '70383c0f',
+  apiSecret: 'UiM45z2qA0PYRemk',
+});
+
+let templateVars;
+
+router.get("/", (req, res) => {
+  const from = '14372661743';
+  const to = '15872204300';
+  const text = 'Sending order to restaurant....';
+
+  nexmo.message.sendSms(from, to, text);
+  res.render("smsSend");
+});
+
+router.get("/smsReceive", (req, res) => {
+  res.render("smsReceive", templateVars);
+})
+
+router.post("/smsReceive", (req, res) => {
+  const params = Object.assign(req.query, req.body)
+  const text = params.text;
+  templateVars = { text };
+  console.log(templateVars);
+  res.redirect("smsReceive", templateVars);
+  res.status(200).send()
+});
+
+router.post("/delivery", (req,res) => {
+  res.status(200).send('received msg');
+});
+
+module.exports = router;
