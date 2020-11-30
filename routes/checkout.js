877d9a6af0dc85ce9a043b88744a1595e138500e
RAFH82 <hrechkar@gmail.com>
2020-11-30 23:32:22 +0000

    added rough text sending functionality of order, to restaurant

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router  = express.Router();
+const Nexmo = require('nexmo');
+
+const nexmo = new Nexmo({
+  apiKey: '70383c0f',
+  apiSecret: 'UiM45z2qA0PYRemk',
+});
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
+
+  router.get("/submit", (req, res) => {
     db.query(`
     SELECT * FROM food_orders
     JOIN orders ON orders.id = order_id
@@ -11,11 +18,22 @@ module.exports = (db) => {
     WHERE users.id = 1
     ;`)
       .then(data => {
+        // res.json({ order });
+        // Order details
+
         const order = data.rows[0];
         const name = order.name;
         const time = order.order_date;
-        console.log(name, time);
-        res.json({ order });
+        const order_id = order.order_id;
+        const templateVars = { order, name, time, order_id };
+        // Sending SMS with order details to restaurant
+        const from = '14372661743';
+        const to = '15872204300';
+        const text = `Order_id: ${order_id}, For: ${name} Time: ${time}`;
+        // Nexmo Sends the sms
+        nexmo.message.sendSms(from, to, text);
+        // Then loads the confirmation page with the order details
+        res.render("confirmation", templateVars);
       })
       .catch(err => {
         res
@@ -23,15 +41,19 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  router.get("/", (req, res) => {
+    res.render("checkout");
+  });
+
   return router;
 };
 
-// router.get("/", (req, res) => {
-//   res.render("checkout");
-// });
 
-// // router.get("/submit", (req, res) => {
-// //   res.render("confirmation");
-// // })
+
+// router.get("/submit", (req, res) => {
+
+
+// })
 
 // module.exports = router;
