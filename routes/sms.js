023e780dcf72a22ee51ffd315df6bf73a3744969
RAFH82 <hrechkar@gmail.com>
2020-12-01 04:59:27 +0000

    Added time conversion for SMS timestamp. Also added SMS response to client once restaurant responds

--- routes/sms.js
+++ routes/sms.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router  = express.Router();
-const Nexmo = require('nexmo');
+const moment = require('moment-timezone');
 
+// For SMS API
+const Nexmo = require('nexmo');
 const nexmo = new Nexmo({
   apiKey: '70383c0f',
   apiSecret: 'UiM45z2qA0PYRemk',
@@ -9,33 +11,33 @@ const nexmo = new Nexmo({
 
 let templateVars;
 
-router.get("/", (req, res) => {
-  const from = '14372661743';
-  const to = '15872204300';
-  const text = 'Sending order to restaurant....';
-
-  nexmo.message.sendSms(from, to, text);
-  res.render("smsSend");
-});
-
 router.get("/smsReceive", (req, res) => {
   if (!templateVars) {
     res.render("orderNotReady")
   } else {
+    const from = '14372661743';
+    const to = '15872204300';
+    const text = `Restaurant Response: ${templateVars.text}. This response was sent at ${templateVars.timeStamp}`;
+    // Nexmo Sends the sms
+    nexmo.message.sendSms(from, to, text);
     res.render("smsReceive", templateVars);
   }
 });
 
+// This is the response from the webhook url when the restaurant responds via SMS
 router.post("/smsReceive", (req, res) => {
-  const params = Object.assign(req.query, req.body)
+  const params = Object.assign(req.body)
   const text = params.text;
-  templateVars = { text };
-  console.log(templateVars);
+  const UTCDateTime = params['message-timestamp'];
+  const timeStamp = (moment.tz(UTCDateTime, "Europe/London")).tz("America/Vancouver").format('YYYY-MM-DD LT');
+  console.log(timeStamp);
+  templateVars = { text, timeStamp };
   res.render("smsReceive", templateVars);
   res.status(200).send()
 });
 
-router.post("/delivery", (req,res) => {
+// Sends delivery status to Nexmo otherwise it will keep retrying
+router.post("/delivery", (req, res) => {
   res.status(200).send('received msg');
 });
 
