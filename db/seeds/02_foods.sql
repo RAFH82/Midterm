7dcea626c84fd8a6a01f8d0ec2e824b5de872569
Samantha Fok <samantha.fok@hotmail.com>
2020-12-02 21:57:03 +0000

    Changed picture of Spaghetti

--- db/seeds/02_foods.sql
+++ db/seeds/02_foods.sql
@@ -10,11 +10,11 @@ INSERT INTO items (name, description, price, food_pic_url) VALUES ('Four Formagg
 
 INSERT INTO items (name, description, price, food_pic_url) VALUES ('Meat Lover','Homemade thin crust pizza, topped off with two types of cheese, bacon, ham, pepperoni and hot sausage!', 50, 'https://cdn.pixabay.com/photo/2017/01/22/19/20/pizza-2000615__340.jpg');
 
-INSERT INTO items (name, description, price, food_pic_url) VALUES ('Spaghetti Bolognese', 'Fresh handmade pasta with a generous helping of our house meat sauce and grated parmesan cheese.', 12.50, 'https://images.unsplash.com/photo-1597933856545-b9ee519aa0ec?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1000&q=80');
+INSERT INTO items (name, description, price, food_pic_url) VALUES ('Spaghetti Bolognese', 'Fresh handmade pasta with a generous helping of our house meat sauce and grated parmesan cheese.', 12.50, 'https://images.unsplash.com/photo-1598866594230-a7c12756260f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=911&q=80');
 
 INSERT INTO items (name, description, price, food_pic_url) VALUES ('Mushroom Penne', 'Fior di Latte, porcini cream, cremini, portobello, parmigiano, topped with a drizzle of pure truffle oil', 15, 'https://images.pexels.com/photos/3214160/pexels-photo-3214160.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
 
-INSERT INTO items (name, description, price, food_pic_url) VALUES ('Seafood Linguine', 'Local jumbo tiger prawns fresh from BC waters and assorted seafood smothered in a mildy spicy and creamy tomato sauce', 40, 'https://images.pexels.com/photos/725997/pexels-photo-725997.jpeg?cs=srgb&dl=pexels-dana-tentis-725997.jpg&fm=jpg');
+INSERT INTO items (name, description, price, food_pic_url) VALUES ('Seafood Linguine', 'Local jumbo tiger prawns fresh from BC waters and assorted seafood smothered in a creamy tomato sauce', 40, 'https://images.pexels.com/photos/725997/pexels-photo-725997.jpeg?cs=srgb&dl=pexels-dana-tentis-725997.jpg&fm=jpg');
 
-INSERT INTO items (name, description, price, food_pic_url) VALUES ('Gnocchi', 'Handmade gnocchi tossed in a buttery cream sauce with fresh spinach and roasted garlic and fresh grated parmesan', 22, 'https://images.unsplash.com/photo-1593003290701-d5e71453a85a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80');
+INSERT INTO items (name, description, price, food_pic_url) VALUES ('Gnocchi', 'Handmade gnocchi tossed in a buttery cream sauce with fresh spinach and fresh grated parmesan', 22, 'https://images.unsplash.com/photo-1593003290701-d5e71453a85a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80');
 -- KEEP DESCRIPTION TO 3 LINES!
