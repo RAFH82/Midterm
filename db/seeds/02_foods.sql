55de83c92a5ea5296d0c53bfcad29e6c043c3ab6
Samantha Fok <samantha.fok@hotmail.com>
2020-12-02 18:33:13 +0000

    Added some some new foods and edited other items

--- db/seeds/02_foods.sql
+++ db/seeds/02_foods.sql
@@ -1,6 +1,9 @@
-INSERT INTO items (name, description, price,food_pic_url) VALUES ('Baguette','A baguette is a long, thin loaf of French bread that is commonly made from basic lean dough. It is distinguishable by its length and crisp crust.', 2.25,'https://www.shutterstock.com/image-photo/french-baguette-white-bread-isolated-on-390074359');
-INSERT INTO items (name, description, price, food_pic_url) VALUES ('cheezePizza','a flat, open-faced baked pie of Italian origin, consisting of a thin layer of bread dough topped with spiced tomato sauce and cheese, often garnished with anchovies, sausage slices, mushrooms, etc.', 30, 'https://cdn.pixabay.com/photo/2016/04/09/09/22/pizza-1317699__340.jpg');
+INSERT INTO items (name, description, price,food_pic_url) VALUES ('Baguette','French bread made the italian way! Our baguette is glazed with hot garlic butter and fresh parsley.', 2.25,'https://images.pexels.com/photos/461060/pexels-photo-461060.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+INSERT INTO items (name, description, price, food_pic_url) VALUES ('Four Formaggio','Basic does not mean boring! A blend of asiago, fior di latte, and fresh mozzarella.', 30, 'https://cdn.pixabay.com/photo/2016/04/09/09/22/pizza-1317699__340.jpg');
 
-INSERT INTO items (name, description, price, food_pic_url) VALUES ('Meat lover','Homemade thin crust pizza, topped off with two types of cheese, bacon, ham, pepperoni and hot sausage!', 50, 'https://cdn.pixabay.com/photo/2017/01/22/19/20/pizza-2000615__340.jpg');
+INSERT INTO items (name, description, price, food_pic_url) VALUES ('Meat Lover','Homemade thin crust pizza, topped off with two types of cheese, bacon, ham, pepperoni and hot sausage!', 50, 'https://cdn.pixabay.com/photo/2017/01/22/19/20/pizza-2000615__340.jpg');
 
 INSERT INTO items (name, description, price, food_pic_url) VALUES ('Spaghetti Bolognese', 'Fresh handmade pasta with a generous helping of our house meat sauce and grated parmesan cheese.', 12.50, 'https://images.unsplash.com/photo-1597933856545-b9ee519aa0ec?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1000&q=80');
+
+INSERT INTO items (name, description, price, food_pic_url) VALUES ('Mushroom Penne', 'Fior di Latte, porcini cream, cremini, portobello, parmigiano, topped with a drizzle of pure truffle oil', 15, 'https://images.pexels.com/photos/3214160/pexels-photo-3214160.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+-- KEEP DESCRIPTION TO 3 LINES!
