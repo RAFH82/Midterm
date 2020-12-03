ccf7632c85f02a4015d58e9080b916f9237d9be3
RAFH82 <hrechkar@gmail.com>
2020-12-03 02:13:42 +0000

    Added db query for users order. Added logic to display item order on confirmation page

--- routes/checkout.js
+++ routes/checkout.js
@@ -15,26 +15,51 @@ module.exports = (db) => {
 
   router.get("/submit", (req, res) => {
     db.query(`
-    SELECT * FROM order_items
+    SELECT users.name AS user,
+    orders.id AS orderId,
+    SUM(order_items.quantity) AS itemQuantity,
+    order_items_total AS totalOrderItems,
+    items.name AS itemName,
+    orders.order_total AS totalOrderCost
+    FROM order_items
     JOIN orders ON orders.id = order_id
     JOIN items ON items.id = item_id
     JOIN users ON users.id = user_id
-    WHERE users.id = 1
+    WHERE orders.id = 1
+    GROUP BY orders.id, users.name, order_items_total, items.name;
     ;`)
       .then(data => {
         // Order details
-        const order = data.rows[0];
-        const name = order.name;
+        let orderArr = [];
+        const order = data.rows;
+        console.log('this is the order object', order);
+        for (let item of order) {
+          orderArr.push({
+            itemQuantity: item.itemquantity,
+            totalCostOfItems: item.totalorderitems,
+            itemName: item.itemname,
+          });
+        }
+        console.log('This is the array', orderArr);
+        // Order fields that will remain the same
+        const name = order[0].user;
+        const order_id = order[0].orderid;
+        const orderTotal = order[0].totalordercost;
+
+        // Time change logic
         const time = Date.now();
         const timeStamp = utcTimeChange(time, "Europe/London", "America/Vancouver");
-        const order_id = order.order_id;
-        const templateVars = { order, name, timeStamp, order_id };
+
+        const templateVars = { orderArr, name, order_id, orderTotal, timeStamp, };
         if (textSent === false) {
           // Sending SMS with order details to restaurant
         const from = '16045952801';
         const to = '15872204300';
         const text = `Order_id: ${order_id}, For: ${name} Time: ${timeStamp}`;
-        textSent = true;
+        // const orderDetails = for (let item of orderArr) {
+        //   text += `${item.itemName} x ${item.itemQuantity}`
+        // }
+        // textSent = true;
         // Nexmo Sends the sms
         nexmo.message.sendSms(from, to, text);
         }
